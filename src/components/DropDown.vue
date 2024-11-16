d80be315c3fc2f6f4bc6b8de933f1706f5dbbe2c
wzy <82089160@qq.com>
2024-11-16 13:17:49 +0800

    feat：DropDown使用hooks自定义函数

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="dropdown">
+    <div class="dropdown" ref="dropdownRef">
       <li class="list-inline-item">
         <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
       </li>
@@ -10,7 +10,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '@/hooks/useClickOutside'
 
 export default defineComponent({
   name: 'DropDown',
@@ -24,12 +25,23 @@ export default defineComponent({
   },
   setup () {
     const isOpen = ref(false)
+    const dropdownRef = ref<null | HTMLElement>(null)
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const isClickOutSide = useClickOutside(dropdownRef)
+
+    watch(isClickOutSide, () => {
+      if (isClickOutSide && isOpen) {
+        isOpen.value = false
+      }
+    })
+
     return {
       isOpen,
-      toggleOpen
+      toggleOpen,
+      // 跟页面上的ref属性完全一样
+      dropdownRef
     }
   }
 })
