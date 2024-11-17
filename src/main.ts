cae96cd78e7cf97d4595dfa5f18a9063185a8e70
wzy <82089160@qq.com>
2024-11-17 22:53:59 +0800

    feat:引用vuex

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { createApp } from 'vue'
 import { createWebHistory, createRouter } from 'vue-router'
+import { createStore } from 'vuex'
 import ColumnDetail from './views/ColumnDetail.vue'
 import Home from './views/Home.vue'
 import App from './App.vue'
 import Login from './views/Login.vue'
+
+const store = createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
+console.log(store.state.count)
+store.commit('add')
+console.log(store.state.count)
 const routerHistory = createWebHistory()
 const router = createRouter({
   history: routerHistory,
