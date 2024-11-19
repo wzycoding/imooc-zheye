9fa55b884351b9e053920ad8d04d5d4d09b69835
wzy <82089160@qq.com>
2024-11-19 23:30:30 +0800

    feat：新建第一个api

--- zheye-api/src/main/java/com/imooc/post/PostController.java
+++ zheye-api/src/main/java/com/imooc/post/PostController.java
@@ -1,7 +1,11 @@
 package com.imooc.post;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/post")
 public class PostController {
 }
--- zheye-api/src/main/java/com/imooc/user/UserController.java
+++ zheye-api/src/main/java/com/imooc/user/UserController.java
@@ -1,4 +1,20 @@
 package com.imooc.user;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户相关接口
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "SUCCESS";
+    }
 }
