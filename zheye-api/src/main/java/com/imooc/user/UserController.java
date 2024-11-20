19dc8080a14089aecdfb14045ead981c9bce90eb
wzy <82089160@qq.com>
2024-11-20 10:18:52 +0800

    feat：测试第一个统一响应接口

--- zheye-api/src/main/java/com/imooc/user/UserController.java
+++ zheye-api/src/main/java/com/imooc/user/UserController.java
@@ -1,5 +1,6 @@
 package com.imooc.user;
 
+import com.imooc.dto.UserInfoDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/login")
-    public String login() {
-
-//        UserInfoDTO userInfoDTO = new UserInfoDTO();
-//        userInfoDTO.setName("wzy");
-//        return userInfoDTO;
-        return "hhhh";
+    public UserInfoDTO login() {
+        UserInfoDTO userInfoDTO = new UserInfoDTO();
+        userInfoDTO.setName("wzy");
+        return userInfoDTO;
     }
 }
